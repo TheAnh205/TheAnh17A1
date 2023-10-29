c3a526ae0ea480c2bc343269e02b0ee15caa75ea
TheAnh205 <148074718+TheAnh205@users.noreply.github.com>
2023-10-29 22:04:42 +0700

    Add files via upload

---
+++ bt2.py
@@ -0,0 +1,11 @@
+#B2
+x=10
+y=5
+c=x+y
+d=x-y
+e=x*y
+f=x/y
+print('Tổng của', x,'+', y, '=', c)
+print('Hiệu của', x,'-', y, '=', d)
+print('Tích của', x,'*', y, '=', e)
+print('Thương của', x,'/', y, '=', f)
